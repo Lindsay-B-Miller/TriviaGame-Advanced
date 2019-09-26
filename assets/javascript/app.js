62c7e6f9a3a027968e4077ad3dc88e71c3edc054
Lindsay <lindsay.lbm@gmail.com>
2019-09-26 15:03:24 -0500

    changed questions to array in js.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,34 @@
 
 // GLOBAL VARIABLES================================================================================================
-var time = 30;
+var time = 10;
 var intervalId;
+
+var questions = [
+    {
+        question: "What is the state dessert of Arizona?",
+        answer: "Lane Cake",
+        option1: "Lane Cake",
+        option2: "Raspberry Popsicles",
+        option3: "Toffee",
+        option4: "Oatmeal Cookies"
+    },
+    {
+        question: "In 1963, which state sold the first fried dill pickle?",
+        answer: "Arkansas",
+        option1: "Kentucky",
+        option2: "New York",
+        option3: "Alabama",
+        option4: "Arkansas"
+    },
+    {
+        question: "Which state is the birthplace of the Cheeseburger?",
+        answer: "Colorado",
+        option1: "Colorado",
+        option2: "Texas",
+        option3: "Idaho",
+        opttion4: "Iowa"
+    }
+]
 // var question1 = document.question1.question1.value;
 
 var temp;
@@ -9,10 +36,14 @@ var temp;
 // When user clicks the start button, start disappears and is replaced by the first question.
 $("#start").click(function start() {
     $("#start").remove();
-    $("#questionOne").show();
-    time = 30;
-    intervalId = setInterval(count, 1000);
+    var q = $("<h2>").text(questions[0].question);
+    $("#question").html(q);
 
+    // $("#question").show();
+    // questions[0].option1, questions[0].option2);
+    time = 10;
+    clearInterval(intervalId);
+    intervalId = setInterval(count, 1000);
 });
 
 //Time counter function
@@ -39,11 +70,11 @@ function check(event) {
     $("#questionOne").remove();
     // $("#results").show();
     document.getElementById("results").style.visibility = "visible";
-
+    setTimeout(buffer, 1000 * 3);
 }
 
 //Time up function
-setTimeout(timeUp, 1000 * 30);
+setTimeout(timeUp, 1000 * 10);
 function timeUp() {
     console.log("time up");
     // notAnswered++;
@@ -52,5 +83,12 @@ function timeUp() {
     // $("#results").show();
     document.getElementById("results").style.visibility = "visible";
     $("#results").text("Time's up! The correct answer was ")
+}
+
+function buffer() {
+    clearInterval(intervalId);
+    time--;
+    intervalId = setInterval(buffer, 1000);
+    alert("buffer done")
 }
 
